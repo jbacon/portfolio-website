14b2991e0aae724e76c5b0bc869519da65fc5f4a
Josh Bacon <jbacon@zagmail.gonzaga.edu>
2020-11-25 09:15:56 -0800

    Fix typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ import Carousel from 'react-material-ui-carousel'
 import { Parallax as Parallax2 } from 'react-parallax';
 
 
-
+// Example of a "styled-component"
 const Text = styled(Typography).attrs(props => ({
     // we can define static props
     variant: "h6",
@@ -44,14 +44,9 @@ class Home extends React.Component {
                             justifyContent: "center",
                             display: "flex",
                             flexGrow: "1",
-                            // flex: "1 0 100%",
-                            // flexWrap: "nowrap",
-                            // alignItems: "stretch",
                             flexDirection: "column",
                         }}>
                             <Parallax2
-                                // bgImage={process.env.PUBLIC_URL + '/images/stuart_mt_2.jpg'}
-                                // bgImageAlt="the cat"
                                 style={{
                                     height: "50vh",
                                     background: 'white',
@@ -60,17 +55,15 @@ class Home extends React.Component {
                                 renderLayer={percentage => (
                                     <div
                                         style={{
-                                            // "url("./images/stuart_mt_2.jpg") 50% -6.89655% / 200% no-repeat"
                                             background: 'url(./images/stuart_mt_2.jpg)',
                                             backgroundRepeat: 'no-repeat',
                                             backgroundPosition: "50% "+(percentage*-1000+910)+"px",
                                             backgroundSize: 'auto 1000px',
                                             position: 'absolute',
-                                            // background: `rgba(255, 125, 0, ${percentage * 1})`,
                                             left: '0px',
                                             top: "0px",
                                             width: "100%",
-                                            height: "100vh"//*percentage * 300,
+                                            height: "100vh"
                                         }}
                                     />
                                 )}>
@@ -82,39 +75,6 @@ class Home extends React.Component {
                                         color: "black", }}>welcome.</Typography>
                                 </FadeIntoView>
                             </Parallax2>
-                            {/* <Parallax strength={2000}>
-                                <Background className="custom-bg">
-                                    <div style={{
-                                        height: "100vh",
-                                        background: 'url(./images/stuart_mt_2.jpg)',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundPosition: "50%",
-                                        backgroundSize: 'cover'}}>
-                                    </div>
-                                </Background>
-                            </Parallax> */}
-                        
-                        {/* <div style={{
-                            height: "50vh",
-                            background: 'white',
-                            overflow: "hidden"}}>
-                            <Parallax y={["100", "-125"]} >
-                                <div style={{
-                                    height: "100vh",
-                                    background: 'url(./images/stuart_mt_2.jpg)',
-                                    backgroundRepeat: 'no-repeat',
-                                    backgroundPosition: "50%",
-                                    backgroundSize: 'cover'}}>
-                                    <FadeIntoView>
-                                        <Typography variant="h3" style={{ 
-                                            display: "block",
-                                            textAlign: "center",
-                                            padding: "4rem",
-                                            color: "black", }}>welcome.</Typography>
-                                    </FadeIntoView>
-                                </div>f
-                            </Parallax>
-                        </div> */}
                         <div style={{
                             minHeight: "50vh",
                             padding: "3rem",
@@ -122,15 +82,12 @@ class Home extends React.Component {
                             <Avatar style={{
                                 width: "150px",
                                 height: "150px",
-                                // position: "absolute",
-                                // bottom: "75px",
                                 right: "calc(-50% + 75px)",
                                 }} src={process.env.PUBLIC_URL + '/images/me.jpg'}/>
                             <Text style={{ }}><br/>keep scrolling</Text>
                             <ExpandMoreIcon style={{
                                 width: "100%",
                             }}/>
-                            {/* <IconButton><ExpandMoreIcon/></IconButton> */}
                         </div>
                         <FadeIntoView>
                             <Text style={{
@@ -186,32 +143,6 @@ class Home extends React.Component {
                         <FadeIntoView>
                             <div style={{
                                 height: "50vh"}}>
-                                <Avatar style={{
-                                    width: "150px",
-                                    height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
-                                    right: "calc(-50% + 75px)",
-                                    }} src={process.env.PUBLIC_URL + '/images/me7.jpg'}/>
-                                <Text>Washington born and raised</Text>
-                            </div>
-                        </FadeIntoView>
-                        <FadeIntoView >
-                            <Text style={{
-                                height: "50vh"}}>
-                            Teamwork is my most important competency.<br/>
-                            I value people and partnerships.<br/>
-                            My work relations are both friendly and effective.<br/>
-                            Learning is a collaborative process,<br/>
-                            and mentorships are given and recieved with gratitude.<br/>
-                            Let's stretch our minds together<br/>
-                            </Text>
-                        </FadeIntoView>
-                        <div style={{
-                                height: "50vh"}}>
-                        <Text>Get To Know Me:</Text>
-                        <Carousel>
-                            <div>
                                 <img style={{
                                     position: "relative",
                                     width: "150px",
@@ -219,14 +150,39 @@ class Home extends React.Component {
                                     right: "calc(-50% + 75px)",
                                     }}
                                     alt="Gonzaga" src={process.env.PUBLIC_URL + '/images/gonzaga.png'}/>
-                                <Text>Gonzaga Alumni</Text>
+                                <Text>
+                                    Gonzaga University Alumni<br/>
+                                    B.S. in Computer Science<br/>
+                                    2015
+                                </Text>
+                            </div>
+                        </FadeIntoView>
+                        <FadeIntoView >
+                            <Text style={{
+                                height: "50vh"}}>
+                            Teamwork is the most important competency.<br/>
+                            I value people and partnerships.<br/>
+                            Learning is a collaborative process,<br/>
+                            and my work relations are both friendly and effective.<br/>
+                            Let's stretch our minds together...<br/>
+                            </Text>
+                        </FadeIntoView>
+                        <div style={{
+                                height: "50vh"}}>
+                        <Text>Get To Know Me:</Text>
+                        <Carousel>
+                            <div>
+                                <Avatar style={{
+                                    width: "150px",
+                                    height: "150px",
+                                    right: "calc(-50% + 75px)",
+                                    }} src={process.env.PUBLIC_URL + '/images/me7.jpg'}/>
+                                <Text>Washington born and raised</Text>
                             </div>
                             <div>
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/me8.jpg'}/>
                                 <Text>Cycling Gears</Text>
@@ -235,8 +191,6 @@ class Home extends React.Component {
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/climbing.jpg'}/>
                                 <Text>Climbing Cliffs</Text>
@@ -245,18 +199,14 @@ class Home extends React.Component {
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/skiing_with_mom.jpg'}/>
-                                <Text>Sking Slopes</Text>
+                                <Text>Skiing Slopes</Text>
                             </div>
                             <div>
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/groupphotohike.jpg'}/>
                                 <Text>Friendly Hikes</Text>
@@ -265,8 +215,6 @@ class Home extends React.Component {
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/me6.jpg'}/>
                                 <Text>Furball Friends</Text>
@@ -275,8 +223,6 @@ class Home extends React.Component {
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/drone.jpg'}/>
                                 <Text>Drone Racing</Text>
@@ -285,8 +231,6 @@ class Home extends React.Component {
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/hummingbird.jpg'}/>
                                 <Text>Art &#38; Photography</Text>
@@ -295,8 +239,6 @@ class Home extends React.Component {
                                 <Avatar style={{
                                     width: "150px",
                                     height: "150px",
-                                    // position: "absolute",
-                                    // bottom: "75px",
                                     right: "calc(-50% + 75px)",
                                     }} src={process.env.PUBLIC_URL + '/images/lakedays.jpg'}/>
                                 <Text>Lake Days</Text>
